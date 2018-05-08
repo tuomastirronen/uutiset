c42327419b335575579acb3cc055276f57987cf7
Tuomas Tirronen <tuomas.tirronen@gmail.com>
2018-05-08 12:40:37 +0300

    bottom distance 400

--- app/assets/javascripts/will_paginate_infinite.js
+++ app/assets/javascripts/will_paginate_infinite.js
@@ -2,7 +2,7 @@ jQuery(function() {
     if ($('.infinite-pagination').size() > 0) {
       $(window).on('scroll', function() {
         var more_posts_url = $('.infinite-pagination a.next_page').attr('href');
-        var bottom_distance = 100;
+        var bottom_distance = 400;
   
         if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - bottom_distance) {
           $('.infinite-pagination').html('Ladataan...');
